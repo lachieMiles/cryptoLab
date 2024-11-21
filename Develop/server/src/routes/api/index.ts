5df4a01cd8dee20c30a76523aacf23cf01d32ba0
shareefevans <shareef3vans@gmail.com>
2024-11-21 21:23:36 +1100

    adding routes for marketAUX

--- Develop/server/src/routes/api/index.ts
+++ Develop/server/src/routes/api/index.ts
@@ -2,9 +2,9 @@ import { Router } from 'express';
 const router = Router();
 
 import cryptoRoutes from './crypto-routes.js';
-// import newsRoutes from './news-routes.js';
+import newsRoutes from './news-routes.js';
 
 router.use('/crypto', cryptoRoutes);
-// router.use('/news', newsRoutes);
+router.use('/news', newsRoutes);
 
 export default router;
