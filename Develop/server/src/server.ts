5c38b34c4a514b2b698dfa28788e37ddbe42842e
henry <henry1192_2004@live.com.au>
2024-11-26 23:31:24 +1100

    change table.tsx code

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -6,24 +6,29 @@ import cors from 'cors';
 import express from 'express';
 import routes from './routes/index.js';
 import { sequelize } from './models/index.js';
+import cryptoRouter from './routes/api/crypto-routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
 app.use(express.static('../client/dist'));
-app.use(cors({
-  origin: 'http://localhost:5173', // Allow requests from your frontend's origin
-  methods: 'GET,POST,PUT,DELETE', // Specify allowed HTTP methods
-  credentials: true, // Allow cookies if needed
-}));
+app.use(cors());
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // Allow requests from your frontend's origin
+    methods: 'GET,POST,PUT,DELETE', // Specify allowed HTTP methods
+    credentials: true, // Allow cookies if needed
+  })
+);
 
 app.use(express.json());
 app.use(routes);
+app.use(express.json());
+app.use('/api/crypto', cryptoRouter);
 
-sequelize.sync({force: forceDatabaseRefresh}).then(() => {
+sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
 });
-
