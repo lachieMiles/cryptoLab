38ed3b4d2a2ee34722f0b5a1261736d27fc4eecf
Lachlan Miles <lachlan_miles13@hotmail.com>
2024-11-26 15:36:25 +1100

    Updating NewsCard

--- Develop/client/src/components/Card.tsx
+++ Develop/client/src/components/Card.tsx
@@ -1,18 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../index.css';
-import { NewsCardProps } from '../interfaces/NewsCard';
+import { NewsCards } from '../interfaces/NewsCard';
 
-const NewsCard: React.FC<NewsCardProps> = ({ title, description, url, }) => {
-    return (
-      <div className="card">
-        <div className="card-details">
-          <h3 className="h3">{title}</h3>
-          <p className="p">{description}</p>
-          <a href={url} target="_blank" rel="noopener noreferrer" className="news-link">
-            Read More
-          </a>
-        </div>
-      </div>
-    );
+const NewsCard: React.FC<NewsCards> = () => {
+
+  const [news, setNews] = useState<NewsCards[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchNews = async () => {
+    try {
+      const newsData = await fetchNewsData(5,0);
+      setNews(newsData);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
+
+  const fetchNewsData = async (limit: number, offset: number) => {
+    try {
+      const response = await fetch(
+        `/news/headlines?filter_entities=true&limit=${limit}&offset=${offset}`
+      );
+      if (!response.ok)
+        throw new Error('Unable to fetch News');
+      const data = await response.json();
+      return data.articles.map((article: any) => ({
+        title: article.title,
+        description: article.descrition,
+        url: article.url,
+      }))
+    } catch (error) {
+      console.error('Unable to fetch News data', error);
+      return[];
+    }
+  };
+
+  useEffect(() => {
+    fetchNews();
+  })
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">Crypto News</div>
+      <ul className="navbar-news">
+        {loading ? (
+          <li>Loading news...</li>
+        ) : (
+          news.map((article, index) => (
+            <li key={index} className="news-item">
+              <a href={article.url} target="_blank" rel="noopener noreferrer">
+                <strong>{article.title}</strong>
+              </a>
+              <p className="news-description">{article.description}</p>
+            </li>
+          ))
+        )}
+      </ul>
+    </nav>
+  );
+};
   export default NewsCard;
