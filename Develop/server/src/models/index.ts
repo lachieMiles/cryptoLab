004fa9f701e6415f04a05b9d65072a8fb47823fd
henry <henry1192_2004@live.com.au>
2024-11-27 23:55:25 +1100

    RISK!

--- Develop/server/src/models/index.ts
+++ Develop/server/src/models/index.ts
@@ -9,6 +9,7 @@ const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
       host: 'localhost',
+      port: Number(process.env.DB_PORT) || 1000,
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
