3300e5a3f6cec0961fd8ea0d6566af58747ad575
Lachlan Miles <lachlan_miles13@hotmail.com>
2024-11-27 17:17:37 +1100

    stashing changes

--- Develop/server/src/routes/api/crypto-routes.ts
+++ Develop/server/src/routes/api/crypto-routes.ts
@@ -4,7 +4,7 @@ const router = Router();
 import cryptoService from '../../service/cryptoService.js';
 
 // GET route => also implementing pagination so we only load the first 10 crypto's
-router.get('/', async (_req: Request, res: Response) => {
+router.get('/dashboard', async (_req: Request, res: Response) => {
   console.log('GET /api/crypto route hit');
   try {
     const start = Number(_req.query.start) || 1;
