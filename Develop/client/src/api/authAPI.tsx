e4e4000b5cfd3a145d41f38abb1332ae781fd1d1
henry <henry1192_2004@live.com.au>
2024-11-28 19:45:27 +1100

    10000

--- Develop/client/src/api/authAPI.tsx
+++ Develop/client/src/api/authAPI.tsx
@@ -1,6 +1,6 @@
 import { UserLogin } from '../interfaces/UserLogin';
 
-const API_BASE_URL = process.env.NODE_ENV === 'production' ? 'https://your-backend.onrender.com' : 'http://localhost:3001'; // Dynamic backend URL
+const API_BASE_URL ='http://localhost:10000'; // Dynamic backend URL
 
 const login = async (userInfo: UserLogin) => {
   try {
